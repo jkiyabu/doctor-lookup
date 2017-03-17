a3e07fc8204274b1881cd7311ceb94047a2760db
Jayeson Kiyabu <jkiyabu@comast.net>
2017-03-16 20:49:25 -0700

    add working api request

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,7 +1,15 @@
+var apiKey = require('./../.env').apiKey;
+
 $(document).ready(function() {
   $('#condition-form').submit(function() {
     event.preventDefault();
     var condition = $('#condition').val();
-    $('#show-doctors').text("You entered " + condition + " as a medical condition");
+    $('#condition').val("");
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + condition + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
+     console.log(JSON.stringify(response));
+    })
+    .fail(function(error){
+      console.log("fail");
+    });
   });
 });
